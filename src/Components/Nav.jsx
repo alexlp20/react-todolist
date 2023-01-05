868a95d337ce9a9802927ff4e9e55be35db60a24
Alejandro Laguna <alex.laguna.perez@gmail.com>
2023-01-05 22:01:36 +0100

    Kept working on the Nav component.

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,33 @@
 import "../CSS/components.css";
+import "../CSS/global.css"
 
-function NavButton(props){
+function NavTab(props){
     return (
-    <button className="navButton"></button>
+    <div className="NavTab">
+        <img src={props.url} />
+        <h3 className="Heading">{props.text}</h3>
+    </div>
+
     );
 }
+
 function Nav(props){
     return (
         <nav>
-            
+            <div className="tabContainer">
+                <NavTab text="Home"/>
+                <NavTab text="School"/>
+                <NavTab text="Work"/>
+            </div>
+            <div className="miscButtons">
+                <h3 style={{
+                    color: "rgb(134, 134, 134)",
+                    borderTop: "solid 2px rgb(47, 47, 47)",
+                    paddingTop: "2%",
+                    display: "flex",
+                    justifyContent: "flex-end"}}
+                    className="Heading">text text text text</h3>
+            </div>
         </nav>
     );
 }
