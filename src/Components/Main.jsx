75ba49eaac599f82de1c650db1447ae7ae0b88b6
Alejandro Laguna <alex.laguna.perez@gmail.com>
2023-01-15 19:23:26 +0100

    Added "Add Task" feature. TODO: Each tab should have independent tasks. I need to figure out how to do it.

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { currentTab } from "../App";
 import Task from "./Task";
 
@@ -11,7 +12,7 @@ const mainStyles = {
 function HeaderOfTaskSection(props){
     return(
         <div 
-        className="AddTaskSection">
+        className="Section">
             <h2 
             style={{
                 width: "25%",
@@ -20,10 +21,12 @@ function HeaderOfTaskSection(props){
                 fontFamily: "'Poppins', sans-serif", 
                 fontWeight: "bolder",
                 boxSizing: "border-box",
-                color: "var(--blue)",
+                color: "var(--mainColor)",
                 backgroundColor: "var(--darker)",
                 textAlign: "center",
-                borderRadius: "0.2em"    
+                borderRadius: "0.2em",
+                paddingBlock: "0.3em",
+                marginBottom: "1em"
             }}
             >{currentTab}   
             </h2>
@@ -32,9 +35,29 @@ function HeaderOfTaskSection(props){
 }
 
 function Main(props){
+    const [taskContent, setTaskContent] = useState("")
+    const [tasks, setTasks] = useState([])
+    function addTask(newTask){        
+        if(newTask === "" || newTask === null){
+            alert("Task title should not be empty.");
+            setTaskContent("");
+            return;
+        }
+        if(newTask.length > 18){
+            alert("Task title is too long.");
+            setTaskContent("");
+            return;
+        }
+        tasks.push(newTask);
+        setTasks(tasks);
+        
+        setTaskContent("");
+
+    }
     return (
         <main style={mainStyles}>
             <HeaderOfTaskSection currentTab={currentTab}/>
+           
             <div className="TaskContainer" style={{
                 display: "flex",
                 flexDirection: "row",
@@ -42,11 +65,37 @@ function Main(props){
 
                 padding: "2vw",
                 height: "auto",
-                borderBottom: "solid 2px var(--blue)",
+                borderTop: "solid 2px rgb(29, 29, 29)",
                 
-            }}>                
-                <Task title="Test Task"/>
-            </div>
+            }}>  
+                <div className="toDoTaskContainer" style={{width: "100%"}}>
+                    <div className="createTaskContainer">
+                        <input style={{
+                            width: "76%", 
+                            height: "3vw", 
+                            fontSize: "1vw",
+                            backgroundColor: "rgb(20, 20, 20)", 
+                            border: "none", 
+                            borderRadius: "0.4em",
+                            paddingInline: "1em",
+                            color: "var(--secondLower)"
+                                }} placeholder="Enter a task..." type="text" value={taskContent} 
+                                onChange={
+                                    /* i access to the input content every time its updated, and update taskContent*/
+                                    (e) => {setTaskContent(e.target.value)}
+                                    }/>
+                        <button id="addBtn" onClick={() => addTask(taskContent)} style={{
+                            marginInlineStart: "1em",
+                            width: "20%",
+                            height: "3vw",
+                            borderRadius: "0.5em",
+                            fontWeight: "600",
+                            fontSize: "1vw",
+                        }}>Add Task</button>
+                    </div>
+                    {tasks.map((i) => {return <Task title={i} />})}
+                </div>        
+            </div>                
         </main>
     );   
 }
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,20 +1,21 @@
 
 const taskStyles = {
     width: "30%",
-    height: "5vh",
+    height: "7vh",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     background: "var(--darker)",
     color: "var(--underline)",
     borderRadius: "0.3em",
-    paddingLeft: "1%",
-    fontSize: "1vw"
+    paddingLeft: "2%",
+    fontSize: "1vw",
+    marginBottom: "1vw"
 }
 function Task(props) {
     return (
         <div style={taskStyles}>
-            <h3>{props.title}</h3>
+            <h3 style={{fontWeight: "normal"}}>{props.title}</h3>
             <div className="icons" style={{marginInline: "3%"}}>
                 <span className="material-symbols-outlined taskIcon">delete</span>
                 <span className="material-symbols-outlined taskIcon">done</span>
